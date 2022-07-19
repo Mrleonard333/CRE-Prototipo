3122004dfbc7aa0b5c352f8974131d782b68b820
Mr.leonard <97007392+Mrleonard333@users.noreply.github.com>
2022-07-19 08:54:44 -0300

    vM_1.6

--- Teste.c
+++ Teste.c
@@ -5,23 +5,23 @@
 
 int main() {
     setlocale(LC_ALL, "Portuguese"); // < Declarando a lingua
-    // ^ Se não funcionar cheque as configurações de sua IDE (Western ISO 8859-1)
+    // ^ Se nÃ£o funcionar cheque as configuraÃ§Ãµes de sua IDE (Western ISO 8859-1)
 
-    // v Declarando funções
+    // v Declarando funÃ§Ãµes
     int options(int *escolha);
     int sim_nao(char sn[10]);
 
     int tf = 0; // < TrueFalse
-    int option = 0; // < Opção do tester
+    int option = 0; // < OpÃ§Ã£o do tester
     int INTarquivo = 0; // < Tamanho da lista
     int INTarquivo_2 = 0; // < Vai guardar o tamanho da lista
 
     char c; // < guarda os caracteres do buffer
-    char SN[10]; // SIM ou NÃO
+    char SN[10]; // SIM ou NÃƒO
     char arquivo[100]; // Nome do arquivo
     char arquivos[INTarquivo][100]; // Nome dos arquivos
 
-    puts("[Fundação crypto] - [CRE_Prototipo_01]\n\a");
+    puts("[FundaÃ§Ã£o crypto] - [CRE_Prototipo_01]\n\a");
     sleep(1); // < Aguarda um segundo
     printf("Iniciar o sistema? [S/N]: ");
     scanf("%s", &SN); // < Input
@@ -29,23 +29,23 @@ int main() {
     // ^ Limpeza do buffer do teclado [Para evitar bugs]
 
     if (sim_nao(SN) == 1) {return 0;}
-            // v Guarda o valor no vetor de opções escolhidas
-    options(&option); // < Ativa a função de opções
+            // v Guarda o valor no vetor de opÃ§Ãµes escolhidas
+    options(&option); // < Ativa a funÃ§Ã£o de opÃ§Ãµes
     sleep(1);
-    puts("\n[! Acentos irão bugar !]\n");
+    puts("\n[! Acentos irÃ£o bugar !]\n");
     sleep(1);
     printf("Deseja afetar mais de um arquivo? [S/N]: ");
-    scanf("%s", &SN); // < Sim ou Não
+    scanf("%s", &SN); // < Sim ou NÃ£o
     while ((c = getchar()) != '\n' && c != EOF){}
     
     // v Se quiser afetar mais de um arquivo
     if (sim_nao(SN) == 0) {
         printf("Digite a quantidade: ");
-        scanf("%d", &INTarquivo); // < Quantidade dos arquivos que serão guradados na lista
+        scanf("%d", &INTarquivo); // < Quantidade dos arquivos que serÃ£o guradados na lista
         while ((c = getchar()) != '\n' && c != EOF){}
 
         INTarquivo_2 = INTarquivo - 1; // < Guarda o tamanho da lista
-        while (INTarquivo != -1) // < enquanto a quantidade dos arquivos não for batida
+        while (INTarquivo != -1) // < enquanto a quantidade dos arquivos nÃ£o for batida
         {   
             INTarquivo--;
             if (INTarquivo == -1) {break;}
@@ -58,27 +58,27 @@ int main() {
         }
         // v Diz que vai afetar mais de um arquivo [True]
         tf = 1;
-    } else { // < Se não quiser
+    } else { // < Se nÃ£o quiser
         printf("Qual o nome do arquivo?: ");
         scanf("%s", &arquivo); // < Guarda o nome do arquivo
         while ((c = getchar()) != '\n' && c != EOF){}}
 
     sleep(1);
-    if (tf != 1) { // Se não for afetar mais de um arquivo
+    if (tf != 1) { // Se nÃ£o for afetar mais de um arquivo
 
-        // v Se a opção [Criar] foi escolhida
+        // v Se a opÃ§Ã£o [Criar] foi escolhida
         if (option == 1) 
-        {   FILE *f1; // < f1 vai apontar o lugar do arquivo na memória
+        {   FILE *f1; // < f1 vai apontar o lugar do arquivo na memÃ³ria
             f1 = fopen(arquivo, "r"); // < Tenta abrir o arquivo
 
                 // v Se conseguir
-            if (f1 != NULL) {puts("[! O arquivo já existe !]"); fclose(f1);} 
+            if (f1 != NULL) {puts("[! O arquivo jÃ¡ existe !]"); fclose(f1);} 
             else {printf("Criadno o arquivo %s...\n", arquivo); fopen(arquivo, "w"); printf("Arquivo %s criado", arquivo);}
         }
-        // v Se a opção [Renomear] foi escolhida
+        // v Se a opÃ§Ã£o [Renomear] foi escolhida
         if (option == 2) 
         {   char novo[100];
-            printf("Qual será o novo nome?: ");
+            printf("Qual serÃ¡ o novo nome?: ");
             scanf("%s", novo);
             while ((c = getchar()) != '\n' && c != EOF){}
 
@@ -86,7 +86,7 @@ int main() {
             if (rename(arquivo, novo) == 0) {printf("Arquivo renomeado para %s", novo);} 
             else {puts("[! Houve um erro !]");}
         }
-        // v Se a opção [Remover] foi escolhida
+        // v Se a opÃ§Ã£o [Remover] foi escolhida
         if (option == 3)
         {
             printf("Apagando o arquivo %s...\n", arquivo);
@@ -97,7 +97,7 @@ int main() {
         }
     } else { // < Se afetar mais de um arquivo
 
-        // Vai fazer o mesmo, porém em massa
+        // Vai fazer o mesmo, porÃ©m em massa
         if (option == 1) 
         {   int INT = INTarquivo_2;
             FILE *f1;
@@ -105,7 +105,7 @@ int main() {
             while (INT != -1) 
             {
                 f1 = fopen(arquivos[INT], "r");
-                if (f1 != NULL) {puts("[! Arquivo já existente !]");}
+                if (f1 != NULL) {puts("[! Arquivo jÃ¡ existente !]");}
                 else {fopen(arquivos[INT], "w"); printf("Arquivo %s criado.\n", arquivos[INT]);}
                 fclose(f1);
                 INT--;
@@ -149,14 +149,14 @@ int main() {
     return 0;
 }
 
-// v Vai checar a resposta de sim ou não
+// v Vai checar a resposta de sim ou nÃ£o
 int sim_nao(char sn[10]) {if (strcmp(sn, "s") == 0 || strcmp(sn, "S") == 0) {return 0;} else {return 1;}}
 
                 // v Guarda a escolha do tester
 int options(int *escolha) {
     char c;
     sleep(1);
-    puts("\n[! Antes de inciar o processo cire uma pasta para o arquivo caso não tenha !]");
+    puts("\n[! Antes de inciar o processo crie uma pasta para o arquivo caso nÃ£o tenha !]");
     sleep(1);
     puts("- [1] CRIAR");
     puts("- [2] REOMEAR");
